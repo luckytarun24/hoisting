5aba0ca388d69c13e677846896c8e9ec138815b9
luckytarun24 <51366078+luckytarun24@users.noreply.github.com>
2023-02-18 13:21:49 +0530

    1 file modified

--- index.js
+++ index.js
@@ -71,3 +71,75 @@ function hoist_7() {
   console.log('Identifier a has already been declared');
 }
 hoist_7();
+
+{
+  var a = 1;
+  //Here the statement a = 10; is no longer changing the value of the global a which remains to be 1, but rather it is changing the local a from a function to an integer value of 10. Since we are logging the global a, the output is 1.
+  function hoist_8() {
+    a = 10;
+    return;
+    function a() {}
+  }
+  hoist_8();
+  console.log('hoist 8 - ', a);
+}
+
+{
+  function hoist_9() {
+    function a() {
+      return 1;
+    }
+    return a();
+    function a() {
+      return 2;
+    }
+  }
+  console.log('hoist 9 - ', hoist_9());
+}
+
+{
+  function hoist_10() {
+    //Now, in such a case of multiple declarations(variable and function in the same scope) with the same identifier, the hoisting of variables is simply IGNORED. The the interpreter comes across the function declaration and hoists it.
+    var a = "I'm a variable";
+    function a() {
+      return "I'm a function";
+    }
+    return a();
+  }
+  console.log('hoist 10 - ', ' hoist_10() - Error: a is not a function');
+}
+
+{
+  function hoist_11() {
+    // let a = "I'm a variable";
+    //Here we know that if in same scope we define same name variable of let and function then it say Identifier 'a' has already been declared.
+    function a() {
+      return "I'm a function";
+    }
+    return a();
+  }
+  console.log(
+    'hoist 11 - ',
+    ' hoist_11() - Error: Identifier a has already been declared'
+  );
+}
+
+function hoist_12() {
+  var a = function () {
+    return 3;
+  };
+  return a();
+  var a = function () {
+    return 8;
+  };
+}
+console.log('hoist 12 -', hoist_12());
+
+{
+  var a = 10;
+  (function () {
+    console.log('hoist 13 - 1 -', 'Original value was: ' + a);
+    var a = 20;
+    console.log('hoist 13 - 2 -', 'New value is: ' + a);
+  })();
+}
